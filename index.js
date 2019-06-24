4cca3e6a51a859829ec49502f23bb6b5d83d75ee
Farah Student <farah.gus@hotmail.com>
2019-06-24 18:56:59 -0400

    finished form

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function validateEmail() {
+    const email = document.getElementById("email").value
+    const confirmEmail = document.getElementById("confirm-email").value
+
+    if (email != confirmEmail) {
+        alert("Emails do not match.")
+    }
+  }
